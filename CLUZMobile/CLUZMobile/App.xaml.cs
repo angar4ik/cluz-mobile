a0c76bc935b83938f7a997ce534ba3ed94cbc456
Anatoly G <4angar@gmail.com>
2020-02-04 16:58:54 -0500

    App Center integration

--- CLUZMobile/CLUZMobile/App.xaml.cs
+++ CLUZMobile/CLUZMobile/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Xamarin.Forms;
 using CLUZ.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace CLUZ
 {
@@ -7,6 +10,9 @@ namespace CLUZ
     {
         public App()
         {
+            AppCenter.Start("android=598860f1-f3c1-48c1-8d53-5a51cd011df2;",
+                  typeof(Analytics), typeof(Crashes));
+
             InitializeComponent();
 
             MainPage = new RegisterPage(); 
